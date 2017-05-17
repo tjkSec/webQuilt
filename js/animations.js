4976c40509563d6bccb58b32935570f538f2c6bb
tjkso <sudozapto@gmail.com>
2017-05-17 20:31:23 +0800

    Started color catagories
    --

--- js/animations.js
+++ js/animations.js
@@ -6,9 +6,21 @@ $( document ).ready(function(){
 });
 
 
-function refresh_data() {
-		$("#overlapping_Tri").animate({left:"-170px"}, 4000).css('overflow', 'visible');
-		setTimeout(function () {
-		$("#overlapping_Tri").animate({left:"-100px"}, 4000).css('overflow', 'visible');
-		}, 3000);
-};
+var refresh_data;
+(function () {
+		var _$0 = this;
+
+		function _0() {
+				$("#overlapping_Tri").animate({
+						left: "-170px"
+				}, 4000).css('overflow', 'visible');
+
+				_$0.setTimeout(function () {
+						$("#overlapping_Tri").animate({
+								left: "-100px"
+						}, 4000).css('overflow', 'visible');
+				}, 3000);
+		}
+
+		refresh_data = _0;
+}).call(this);
