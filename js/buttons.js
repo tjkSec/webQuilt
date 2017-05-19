85c242d4e5a142422a9c66b3a4505b5e61b3b523
tjkso <sudozapto@gmail.com>
2017-05-19 16:17:39 +0800

    Added scaling feature
    - no longer need side nav

---
+++ js/buttons.js
@@ -0,0 +1,17 @@
+
+function mid() {
+	$('.color_set-1').animate({width:'75vw'});
+}
+
+function max() {
+	$('.color_set-1').animate({width:'100vw'});
+}
+
+function half() {
+	$('.color_set-1').animate({width:'50vw'});
+}
+
+
+// $('.color_set-1').css({width:'50vh'});
+// left:50%;
+// transform: translate(-50%, -50%);
